c55e791e4643d187ddbdfdf9ee158bf3fb6ec17a
Divy Patel <pdivy2002@gmail.com>
2023-11-20 10:22:29 +0530

    new

--- build/index.js
+++ build/index.js
@@ -14,65 +14,60 @@ var provider = 'https://auth.riotgames.com', authorizeUrl = provider + '/authori
 app.get('/oauth', (req, res) => {
     var accessCode = req.query.code;
     // Display a message before redirecting
-    var redirectingMessage = `<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-  </head>
-  <body style="margin: 0">
-    <div style="justify-content: center; align-items: center; display: inline-grid; margin: 20px 10px 20px 10px">
-      <img src="https://arcadeapp.site/assets/images/arcade-logo.jpg" alt="#" style="width: 200px; height: auto" />
-      <p style="font-size: 32px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: 600">
-        Redirecting to app. Please wait...
-      </p>
-    </div>
-
-    <div style="margin: 20px 10px 20px 10px; padding: 20px 10px; background-color: beige">
-      <h3 style="margin: 0; text-transform: uppercase; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">Note</h3>
-      <p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; margin: 10px 0">Press Continue if you see this message.</p>
-      <img src="https://arcadeapp.site/assets/images/notemessage.jpg" alt="#" style="width: 230px; height: auto" />
-    </div>
-  </body>
-</html>`;
-    // Send the initial response with the message
-    res.send(redirectingMessage);
+    //   var redirectingMessage = `<html>
+    //   <head>
+    //     <meta charset="utf-8" />
+    //     <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    //     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    //   </head>
+    //   <body style="margin: 0">
+    //     <div style="justify-content: center; align-items: center; display: inline-grid; margin: 20px 10px 20px 10px">
+    //       <img src="https://arcadeapp.site/assets/images/arcade-logo.jpg" alt="#" style="width: 200px; height: auto" />
+    //       <p style="font-size: 32px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: 600">
+    //         Redirecting to app. Please wait...
+    //       </p>
+    //     </div>
+    //     <div style="margin: 20px 10px 20px 10px; padding: 20px 10px; background-color: beige">
+    //       <h3 style="margin: 0; text-transform: uppercase; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">Note</h3>
+    //       <p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; margin: 10px 0">Press Continue if you see this message.</p>
+    //       <img src="https://arcadeapp.site/assets/images/notemessage.jpg" alt="#" style="width: 230px; height: auto" />
+    //     </div>
+    //   </body>
+    // </html>`;
+    //   // Send the initial response with the message
+    //   res.send(redirectingMessage);
     // Delay the redirect by 3 seconds
-    setTimeout(() => {
-        // make server-to-server request to token endpoint
-        // exchange authorization code for tokens
-        request_1.default.post({
-            url: tokenUrl,
-            auth: {
-                user: clientID,
-                pass: clientSecret
-            },
-            form: {
-                grant_type: 'authorization_code',
-                code: accessCode,
-                redirect_uri: appCallbackUrl
-            }
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                // parse the response to JSON
-                var payload = JSON.parse(body);
-                // separate the tokens from the entire response body
-                var tokens = {
-                    refresh_token: payload.refresh_token,
-                    id_token: payload.id_token,
-                    access_token: payload.access_token
-                };
-                console.log('Tokens Generated and Redirected', tokens);
-                // legibly print out our tokens
-                const redirectUri = 'arcadeauth://oauth2redirect';
-                const redirectUrl = `${redirectUri}?tokens=${JSON.stringify(tokens)}`;
-                res.redirect(302, redirectUrl);
-            }
-            else {
-                res.send('/token request failed error: ' + error);
-            }
-        });
-    }, 500); // 3000 milliseconds (3 seconds) delay
+    request_1.default.post({
+        url: tokenUrl,
+        auth: {
+            user: clientID,
+            pass: clientSecret
+        },
+        form: {
+            grant_type: 'authorization_code',
+            code: accessCode,
+            redirect_uri: appCallbackUrl
+        }
+    }, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            // parse the response to JSON
+            var payload = JSON.parse(body);
+            // separate the tokens from the entire response body
+            var tokens = {
+                refresh_token: payload.refresh_token,
+                id_token: payload.id_token,
+                access_token: payload.access_token
+            };
+            console.log('Tokens Generated and Redirected', tokens);
+            // legibly print out our tokens
+            const redirectUri = 'arcadeauth://oauth2redirect';
+            const redirectUrl = `${redirectUri}?tokens=${JSON.stringify(tokens)}`;
+            res.redirect(302, redirectUrl);
+        }
+        else {
+            res.send('/token request failed error: ' + error);
+        }
+    });
 });
 // Website Data
 // --------------------------------
